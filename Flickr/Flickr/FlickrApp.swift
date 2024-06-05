5ee8501686c6a59815cbdd1b975ff5d7bc30b27d
Tiago Schernoveber <tiago.schernoveber@gmail.com>
2024-06-05 12:44:05 -0300

    [FLICKR] Project creation MVVM sctructure

---
+++ Flickr/Flickr/FlickrApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrApp.swift
+//  Flickr
+//
+//  Created by Tiago Schernoveber on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
